845fb84ec364201a2e7c37c08215077f6d5e1aa9
Vladylcew <vlad30444@yandex.ru>
2022-12-14 20:33:03 +0300

    Adding 9th Task

---
+++ 9th_Task/FFiMA/FFiMA/Executable.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include"FFiMA.h"
+using namespace std;
+
+
+int main() {
+	FIFO<char,string> C;
+	C.push('x');
+	C.push('y');
+	C.push('z');
+	char a, b, c;
+	a = C.get();
+	b = C.get();
+	c = C.get();
+	cout << a << "\n" << b<<"\n"<<c<<"\n";
+	
+
+	FIFO<int> A;
+	A.push(1);
+	A.push(5);
+	A.push(4);
+
+	cout << A.get() << " ";
+	cout << A.get() << " ";
+	cout << A.get();
+
+
+}
---
+++ 9th_Task/FFiMA/FFiMA/FFiMA.h
@@ -0,0 +1,76 @@
+﻿#include <deque>
+#include <vector>
+#include <string>
+#include <iostream>
+
+template <class T, class Container_T = std::deque <T>>
+	class FIFO {
+	public:
+		FIFO() = default;
+
+		bool empty() { return !container.size(); }
+		void push(const T& el) {
+			container.push_back(el);
+		}
+		T get() {
+			if (!empty())
+			{
+				T tmp = container.front();
+				container.pop_front();
+				return tmp;
+			}
+			return NULL;
+		}
+	private:
+		Container_T container;
+};
+
+
+/////////////////VECTOR////////////////////
+template <class T>
+	class FIFO<T,std::vector<T>> {
+	public:
+		FIFO() = default;
+
+		bool empty() { return !container.size(); }
+		void push(const T& el) {
+			container.push_back(el);
+		}
+
+		T get() {
+			T tmp = container[0];
+			for (int i = 0; i < container.size()-1; i++) {
+				container[i] = container[i + 1];
+			}
+			container.resize(container.size() - 1);
+			return tmp;
+		}
+
+	private:
+		std::vector<T> container;
+};
+
+/////////////////STRING////////////////////
+template <>
+class FIFO<char, std::string> {
+public:
+	FIFO() = default;
+
+	bool empty() { return !container.size(); }
+	void push(const char el) {
+		container += el;
+	//std::cout << container;
+	}
+	char get() {
+		if (!empty())
+		{
+			char tmp = container[0];
+			container = container.substr(1, container.size() - 1);
+			return tmp;
+		}
+		return NULL;
+	}
+
+private:
+	std::string container;
+};
