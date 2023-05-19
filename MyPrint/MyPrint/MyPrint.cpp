48e697621832626756acf8d356797f2aaf6b12fa
Vladylcew <vlad30444@yandex.ru>
2023-05-19 17:08:01 +0300

    321

---
+++ MyPrint/MyPrint/MyPrint.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+void print() {
+	cout << endl;
+}
+
+template <typename Head, typename...Tail>
+void print(Head&& head, Tail&&... tail) {
+	cout <<head  << " ";
+	print(tail...);
+}
+
+int main() {
+	int b = 5;
+	int& a = b;
+	int c = -54;
+	print(1, 5.4, -2 ,"f g", a);
+	print(1, 5.4, -2, "f g",b, move(c));
+}
